b7b26856570210528f59d2e6e67bbf325f9b65db
marlowdickel <42848987+marlowdickel@users.noreply.github.com>
2021-09-13 16:54:21 -0300

    versão funcional

--- fila.c
+++ fila.c
@@ -5,101 +5,112 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-Fila* cria(int tItem) {
+Fila* cria() {
     Fila* fila = (Fila*) malloc(sizeof(Fila));
     if (fila){
         fila->proximoLugar = 0;
-        fila->inicio = NULL;
+        fila->inicio = -1;
         int i;
         for(i=0; i < TAM_MAX; i++)
         {
             fila->tamanhos[i] = malloc(sizeof(int));
-            //fila->tipos[i] = malloc(10 * sizeof(char));
             if(fila->tamanhos[i] == NULL)
             {
                 for(--i;i>0;i--)
                 {
                     free(fila->tamanhos[i]);
-                    //free(fila->tipos[i]);
                 }
                 free(fila->tamanhos);
-                //free(fila->tipos);
                 free(fila);
                 fila = NULL;
-                printf("Não criou a Fila\n");
+                printf("NÃ£o criou a Fila\n");
             }
         }
     } else {
-        printf("Não criou a Fila\n");
+        printf("NÃ£o criou a Fila\n");
     }
     return fila;
 }
 
-int destroi(Fila* fila) {//não alterado corpo
+int destroi(Fila* fila) {
     int i;
-    if(!IsEmpty(fila)){
-        for(i=0;i<fila->topo;i++){
-            free(fila->itens[i]);
+    if(!estaVazia(fila)){
+        for(i=0;i<TAM_MAX;i++){
+            if (fila->tamanhos[i]!=NULL){
+                free(fila->itens[i]);
+            }
         }
     }
     free(fila);
-    printf("Destroi a Fila\n");
+    //printf("Destroi a Fila\n");
+    return TRUE;
 }
 
 int estaCheia(Fila* fila) {
-    if (fila->proximoLugar == fila->inicio) return TRUE;
-    else return FALSE;
+    //printf("prox: %d\n", fila->proximoLugar);
+    //printf("inicio: %d\n", fila->inicio);
+    if (fila->proximoLugar == fila->inicio){
+        //printf("t \n");
+        return TRUE;
+    } else {
+        //printf("f \n");
+        return FALSE;
+    }
 }
 
 int estaVazia(Fila* fila) {
-    if (fila->inicio == NULL) return TRUE;
-    else return FALSE;
-}
-
-int empilha(void *ponteiroItem, int tamanhoItem, Fila* fila){
-    if (!estaCheia(fila)) {
-        fila->itens[fila->proximoLugar] = (void*) malloc(tamanhoItem);
-        fila->tamanhos[fila->proximoLugar] = tamanhoItem;
-        memcpy(fila->itens[fila->proximoLugar], ponteiroItem, tamanhoItem);
-        if(fila->inicio==NULL)
-            fila->inicio = fila->proximoLugar;
-        incrementa(fila->proximoLugar);
+    if (fila->inicio == -1){
         return TRUE;
     } else {
         return FALSE;
     }
 }
 
-int desempilha(Fila* fila) {
-    if (!estaVazia(fila)) {
-        fila->tamanhos[fila->inicio]) = NULL;
-        free(fila->itens[fila->inicio]);
-        incrementa(fila->inicio);
-        if(fila->proximoLugar == fila->inicio){
-            fila->inicio = NULL;
+int enfilera(void *ponteiroItem, int tamanhoItem, Fila* fila){
+    //printf("ponteiroitem: %d\n", ponteiroItem);
+    //printf("ponteiro valor: %d\n", (int) &ponteiroItem);
+    if (!estaCheia(fila)) {
+        fila->itens[fila->proximoLugar] = (void*) malloc(tamanhoItem);
+        fila->tamanhos[fila->proximoLugar] = tamanhoItem;
+        memcpy(fila->itens[fila->proximoLugar], ponteiroItem, tamanhoItem);
+//        printf("Na fila: %d\n", fila->itens[fila->proximoLugar]);
+//        printf("ponteiroItem: %d\n", ponteiroItem);
+        if(fila->inicio == -1){
+            fila->inicio = fila->proximoLugar;
         }
+        fila->proximoLugar = incrementa(fila->proximoLugar);
+//        printf("inicio vai para: %d\n", fila->inicio);
+//        printf("proximo vai para: %d\n", fila->proximoLugar);
         return TRUE;
-        /*
-        void* elRemovido = malloc(fila->tamItem);
-        memcpy(elRemovido, fila->itens[fila->topo - 1], fila->tamItem);
-        fila->topo--;
-        return elRemovido;
-        */
+    } else {
+        return FALSE;
+    }
+}
+
+int desenfilera(Fila* fila) {
+    if (!estaVazia(fila)) {
+        fila->tamanhos[fila->inicio] = NULL;
+        free(fila->itens[fila->inicio]);
+        fila->inicio = incrementa(fila->inicio);
+        if(fila->proximoLugar == fila->inicio){
+            fila->inicio = -1;
+        }
+ //       printf("inicio ao desenf vai para: %d\n", fila->inicio);
+ //       printf("proximo ao desenf vai para: %d\n", fila->proximoLugar);
+        return TRUE;
     } else return FALSE;
 }
 
-void* getProximo(Fila* fila) {//não alterado corpo
-    if (!IsEmpty(fila)) {
-        void* elTopo = malloc(fila->tamItem);
-        memcpy(elTopo, fila->itens[fila->topo - 1], fila->tamItem);
-        return elTopo;
-    } else return NULL;
-
+int getProximo (Fila* fila, void *elemento){
+    if(estaVazia(fila)) return FALSE;
+    memcpy(elemento,fila->itens[fila->inicio],fila->tamanhos[fila->inicio]);
+    return TRUE;
 }
 
 int incrementa(int i){
+    //       printf("incrementa recebeu: %d\n", i);
     if (i==TAM_MAX-1){
         return 0;
     }
-    return i++;
+    return i+1;
 }
