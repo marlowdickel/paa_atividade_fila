76395dc00358b3e3d96d31746c388c841acc2511
marlowdickel <42848987+marlowdickel@users.noreply.github.com>
2021-09-13 16:51:41 -0300

    versão funcional

--- aplicacao.c
+++ aplicacao.c
@@ -1,119 +1,127 @@
 #include "aplicacao.h"
+#include "pilha.c"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stddef.h>
 
-void VeTopoInt(Pilha* pilha) {
-    void* item = Top(pilha);
-    printf("Topo int");
-    printf("topo: %d\n", ((ItemInt*)Top(pilha))->info);
-}
-
-void VeTopoFlo(Pilha* pilha) {
-    void* item = Top(pilha);
-    printf("Topo Float");
-    printf("topo: %f\n", ((ItemFloat*)Top(pilha))->info);
-}
-
-void VeTopoChar(Pilha* pilha) {
-    printf("Topo char");
-    printf("%s\n", ((ItemChar*)Top(pilha))->info);
-}
-
-
-
-void EsvaziaPilhaInt(Pilha* pilha) {
-    while(!IsEmpty(pilha)) {
-        printf("%d\n", ((ItemInt*)Pop(pilha))->info);
-    }
-}
-
-void EsvaziaPilhaFlo(Pilha* pilha) {
-    while(!IsEmpty(pilha)) {
-        printf("%f\n", ((ItemFloat*)Pop(pilha))->info);
-    }
-}
-
-void EsvaziaPilhaChar(Pilha* pilha) {
-    while(!IsEmpty(pilha)) {
-        printf("%s\n",((ItemChar*)Pop(pilha))->info);
-    }
-}
-
-void addElementoInt(int* v, Pilha* p) {
- //   ItemInt* i = (ItemInt*) malloc(sizeof(ItemInt));
-   // i->info = v;
-    //printf("%d\n", i->info);
-    Push((void*) v, p);
-}
-void addElementoChar(char* s, Pilha* p) {
-//    ItemChar* i = (ItemChar*) malloc(sizeof(ItemChar));
-  //  strcpy(i->info, s);
-    //printf("%s\n", s);
-    Push((void*) s, p);
-}
-
-void addElementoFlo(float* vf, Pilha* p) {
-//    ItemChar* i = (ItemChar*) malloc(sizeof(ItemChar));
-  //  strcpy(i->info, s);
-    //printf("%s\n", s);
-    Push((void*) vf, p);
-}
-
 int main()
 {
-    //vai precisar de um vetor pra salvar os tipos armazenados na fila
-    Pilha* P1 = Init(sizeof(ItemInt));
-    Pilha* P2 = Init(sizeof(ItemChar));
-    Pilha* P3 = Init(sizeof(ItemFloat));
-    printf("Pilha de Inteiros: \n");
-    int valor = 7;
-    int valor2 = 6;
-    int valor3 = 5;
-    int valor4 = 4;
-    printf("conteudo vari: %d\n",valor);
-    printf("endereco vari: %p\n",&valor);
-    addElementoInt(&valor, P1);
-    Pop(P1);
+    int ret; // para os retornos
+    int i; // para os "for"
+    int intSaida; // para os valores retirados das pilhas
+    pPilha pilhaSaida; // para retirar as pilhas da fila
 
-    addElementoInt(&valor2, P1);
-    addElementoInt(&valor3, P1);
-    addElementoInt(&valor4, P1);
-    valor4 = 100;
-    printf("Elementos da Pilha de Inteiros: \n");
+    // OK - Crie uma fila circular c/ 3 posições
+    printf("AÇÃO - Crie uma fila circular c/ 3 posições\n\n");
+    Fila* minhaFila = cria();
+    printf("Fila criada\n");
 
-    EsvaziaPilhaInt(P1);
+    // OK - Crie 1 pilha c/ 10 posições
+    printf("AÇÃO - Crie 1 pilha c/ 10 posições\n\n");
+    pPilha pilha1;
+    ret = criapilha(&pilha1,10,sizeof(int));
+    if (!ret) {
+        printf ("ERRO cria pilha 1!\n");
+    } else printf ("Pilha 1 criada!\n");
 
-    printf("\n\n");
-    printf("Pilha de String: \n");
+    // OK - Empilhe (0,1, ... 8)
+    printf("AÇÃO - Empilhe (0,1, ... 8)\n\n");
+    for (i=0; i<=8; i++){
+        ret = empilha(pilha1, &i);
+        if (!ret) {
+            printf ("ERRO empilha valor %d!\n", i);
+        } else printf ("Valor %d empilhado!\n", i);
 
-    printf("Elementos da Pilha de String: \n");
-    addElementoChar("A", P2);
+    }
 
-    VeTopoChar(P2);
-    addElementoChar("B", P2);
-    Pop(P2);
-    addElementoChar("C", P2);
-    VeTopoChar(P2);
-    addElementoChar("D", P2);
+    // OK - enfileire a pilha
+    printf("AÇÃO - enfileire a pilha\n\n");
+    ret = enfilera(&pilha1, sizeof(pilha1), minhaFila);
+    if (!ret) {
+        printf ("ERRO enfilera pilha 1!\n");
+    } else printf ("Valor pilha 1 enfilerado!\n");
 
-    EsvaziaPilhaChar(P2);
+    // OK - Crie 1 pilha c/ 10 posições
+    printf("AÇÃO - Crie 1 pilha c/ 10 posições\n\n");
+    pPilha pilha2;
+    ret = criapilha(&pilha2,10,sizeof(int));
+    if (!ret) {
+        printf ("ERRO cria pilha 2!\n");
+    } else printf ("Pilha 2 criada!\n");
 
-    printf("Pilha de Float: \n");
-    float valor5 = 7.14;
-    float valor6 = 6.50;
-    float valor7 = 5.45;
-    float valor8 = 4.89;
-    addElementoFlo(&valor5, P3);
-    Pop(P3);
-    addElementoFlo(&valor6, P3);
-    addElementoFlo(&valor7, P3);
-    addElementoFlo(&valor8, P3);
-    printf("Elementos da Pilha de Float: \n");
+    // OK - Empilhe (8,7, ... 0)
+    printf("AÇÃO - Empilhe (8,7, ... 0)\n\n");
+    for (i=8; i>=0; i--){
+        ret = empilha(pilha2, &i);
+        if (!ret) {
+            printf ("ERRO empilha valor %d!\n", i);
+        } else printf ("Valor %d empilhado!\n", i);
 
-    EsvaziaPilhaFlo(P3);
+    }
+
+    // OK - enfileire a pilha
+    printf("AÇÃO - enfileire a pilha\n\n");
+    ret = enfilera(&pilha2, sizeof(pilha2), minhaFila);
+    if (!ret) {
+        printf ("ERRO enfilera pilha 2!\n");
+    } else printf ("Valor pilha 2 enfilerado!\n");
+
+    // OK - Desempilhe 4 elementos
+    printf("AÇÃO - Desempilhe 4 elementos\n\n");
+    getProximo(minhaFila, &pilhaSaida);
+    for (i = 1; i<=4; i++){
+        ret = desempilha(pilhaSaida, &intSaida);
+        if (!ret)
+            printf ("ERRO desempilha %d...\n", intSaida);
+        else
+            printf ("OK desempilha %d\n", intSaida);
+    }
+
+    // OK - desenfileire a pilha
+    printf("AÇÃO - desenfileire a pilha\n\n");
+    ret = desenfilera(minhaFila);
+    if (!ret) {
+        printf ("ERRO desenfilera pilha 1!\n");
+    } else printf ("Valor pilha 1 desenfilerado!\n");
+
+    // OK - Imprima o topo da pilha retornada
+    printf("AÇÃO - Imprima o topo da pilha retornada\n\n");
+    getProximo(minhaFila, &pilhaSaida);
+    ret = topo(pilhaSaida,&i);
+    if (!ret)
+        printf ("ERRO topo ...\n");
+    else
+        printf ("OK topo %d\n", i);
+        for (i = 1; i<=4; i++){
+        ret = desempilha(pilhaSaida, &intSaida);
+        if (!ret)
+            printf ("ERRO desempilha %d...\n", intSaida);
+        else
+            printf ("OK desempilha %d\n", intSaida);
+    }
+
+    // OK - Desenfileire a pilha
+    printf("AÇÃO - Desenfileire a pilha\n\n");
+    ret = desenfilera(minhaFila);
+    if (!ret) {
+        printf ("ERRO desenfilera pilha 2!\n");
+    } else printf ("Valor pilha 2 desenfilerado!\n");
+
+    // OK - Destrua a pilha
+    printf("AÇÃO - Destrua a pilha\n\n");
+    ret = destroipilha(&pilhaSaida);
+    if (!ret) {
+        printf ("ERRO destroi pilha 2!\n");
+    } else printf ("Pilha 2 destruida!\n");
+
+    // OK - Destrua a fila
+    printf("AÇÃO - Destrua a fila\n\n");
+    ret = destroi(minhaFila);
+    if (!ret) {
+        printf ("ERRO destroi fila 2!\n");
+    } else printf ("Fila destruida!\n");
 
     return 0;
 }
