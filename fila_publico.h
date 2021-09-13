1e34eb701f5715e859fc9e30bedf163be45459cb
marlowdickel <42848987+marlowdickel@users.noreply.github.com>
2021-09-13 16:53:07 -0300

    versão funcional

--- fila_publico.h
+++ fila_publico.h
@@ -4,28 +4,17 @@
 #include <stddef.h>
 
 
-#ifndef PILHA_PUB_H
-#define PILHA_PUB_H
+#ifndef FILA_PUBLICO_H
+#define FILA_PUBLICO_H
 
 typedef struct mFilas Fila;
 
 Fila* cria();
 int destroi(Fila* fila);
-int estaVazia(Fila* fila)
-int estaCheia(Fila* fila)
-int empilha(void *ponteiroItem, int tamanhoItem, Fila* fila)
-int desempilha(Fila* fila)
-void* getProximo(Fila* fila)
+int estaVazia(Fila* fila);
+int estaCheia(Fila* fila);
+int enfilera(void *ponteiroItem, int tamanhoItem, Fila* fila);
+int desenfilera(Fila* fila);
+int getProximo (Fila* fila, void *elemento);
 
-/*
-typedef struct mPilhas Pilha;
-
-Pilha* Init(int tItem);
-void Destroy(Pilha* pilha);
-int IsEmpty(Pilha* pilha);
-int IsFull(Pilha* pilha);
-void Push(void *ponteiroItem, Pilha* pilha);
-void* Pop(Pilha* pilha);
-void* Top(Pilha* pilha);
-*/
 #endif
